2a4ce414f6736092f4f4fae8e50c088fae16b94d
xavi <xavi@IF4L-PROFE.ASIR.IESRODEIRA.COM>
2018-02-01 18:31:18 +0100

    Inicial

--- Pedido/src/pedido/Pedido.java
+++ Pedido/src/pedido/Pedido.java
@@ -8,6 +8,25 @@ package pedido;
 import java.io.File;
 import java.util.ArrayList;
 
+/*##    PEDIDO      ##
+Número de pedido: { 20304 }
+Cliente: { Muebles Bonitos S.A. }
+Código del cliente: { 00293 }
+Dirección de factura: { C/ De en frente, 11 }
+} Dirección de entrega: { C/ De al lado, 22 }
+Nombre del contacto: { Elias }
+Teléfono del contacto: { 987654321 }
+Correo electrónico del contacto: { mail@mail1234.com }
+Fecha preferente de entrega: { 19/11/2012 }
+Forma de pago: { Transferencia }
+## ARTICULOS ##
+{ Código Artículo | Descripción | Cantidad }
+{ 0001231 | Tuercas tipo 1 | 200 }
+{ 0001200 | Tornillos tipo 1 | 200 }
+{ 0002200 | Arandelas tipo 2 | 200 }
+## FIN ARTICULOS ##
+## FIN PEDIDO ##*/
+
 /**
  *
  * @author xavi
@@ -18,6 +37,9 @@ public class Pedido {
 
 
     public Pedido(String pedido) {
+        String regex="##[ ]*ARTICULOS[ ]*##";
+        String[] partes=pedido.split(regex);
+        for(String s: partes) System.out.println(s);
     }
 
     public Pedido(File pedido) {
@@ -46,6 +68,26 @@ public class Pedido {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String ejemplo="##    PEDIDO      ##\n" +
+"Número de pedido: { 20304 }\n" +
+"Cliente: { Muebles Bonitos S.A. }\n" +
+"Código del cliente: { 00293 }\n" +
+"Dirección de factura: { C/ De en frente, 11 }\n" +
+"} Dirección de entrega: { C/ De al lado, 22 }\n" +
+"Nombre del contacto: { Elias }\n" +
+"Teléfono del contacto: { 987654321 }\n" +
+"Correo electrónico del contacto: { mail@mail1234.com }\n" +
+"Fecha preferente de entrega: { 19/11/2012 }\n" +
+"Forma de pago: { Transferencia }\n" +
+"## ARTICULOS ##\n" +
+"{ Código Artículo | Descripción | Cantidad }\n" +
+"{ 0001231 | Tuercas tipo 1 | 200 }\n" +
+"{ 0001200 | Tornillos tipo 1 | 200 }\n" +
+"{ 0002200 | Arandelas tipo 2 | 200 }\n" +
+"## FIN ARTICULOS ##\n" +
+"## FIN PEDIDO ##";
+        Pedido p=new Pedido(ejemplo);
+        System.out.println(p);
     }
     
 }
